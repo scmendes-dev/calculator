474e6397eb3ec36987b843755b56e7637093d360
Sara Mendes <scmendes.m@gmail.com>
2021-04-03 22:01:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function calc() {
+    let a = parseInt(document.querySelector("#value1").value);
+    let b = parseInt(document.querySelector("#value2").value);
+    let op = document.querySelector("#value2").value;
+}
