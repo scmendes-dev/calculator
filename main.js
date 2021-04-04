c1d7c7e83459e0f485d88cc643545aca0b1a6ae4
Sara Mendes <scmendes.m@gmail.com>
2021-04-04 22:31:28 +0200

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 function calc() {
     let a = parseInt(document.querySelector("#value1").value);
     let b = parseInt(document.querySelector("#value2").value);
-    let op = document.querySelector("#value2").value;
+    let op = document.querySelector("#Operator").value;
+    let calculate;
+
+    if (op == "Add") {
+        calculate = a + b;
+    } else if (op == "Min") {
+        calculate = a - b;
+    } else if (op == "Div") {
+        calculate = a / b;
+    } else if (op == "Mul") {
+        calculate = a * b;
+    } 
+
+  
+    document.querySelector("#result").innerHTML = calculate;
+  
 }
